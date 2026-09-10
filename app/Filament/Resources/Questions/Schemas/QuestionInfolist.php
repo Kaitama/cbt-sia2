141e3a6a012da132935aa2a01da0c8c887449739
kaitama <mr.ibnutama@gmail.com>
2026-09-10 11:02:34 +0700

    Pertemuan 11: Filters

--- app/Filament/Resources/Questions/Schemas/QuestionInfolist.php
+++ app/Filament/Resources/Questions/Schemas/QuestionInfolist.php
@@ -2,9 +2,14 @@
 
 namespace App\Filament\Resources\Questions\Schemas;
 
+use App\Models\Question;
+use Dom\Text;
 use Filament\Infolists\Components\IconEntry;
 use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
+use Filament\Support\Colors\Color;
+use Filament\Support\Enums\TextSize;
 
 class QuestionInfolist
 {
@@ -12,21 +17,43 @@ class QuestionInfolist
     {
         return $schema
             ->components([
-                TextEntry::make('subject.name')
-                    ->label('Subject')
-                    ->placeholder('-'),
-                TextEntry::make('score')
-                    ->numeric(),
-                TextEntry::make('payload')
-                    ->columnSpanFull(),
-                IconEntry::make('is_active')
-                    ->boolean(),
-                TextEntry::make('created_at')
-                    ->dateTime()
-                    ->placeholder('-'),
-                TextEntry::make('updated_at')
-                    ->dateTime()
-                    ->placeholder('-'),
+                Section::make()
+                    ->columns(4)
+                    ->columnSpanFull()
+                    ->schema([
+                        TextEntry::make('subject.name')
+                            ->label('Mata pelajaran')
+                            ->columnSpan(2)
+                            ->placeholder('-'),
+                        TextEntry::make('score')
+                            ->label('Bobot nilai')
+                            ->suffix(' poin')
+                            ->numeric(),
+                        IconEntry::make('is_active')
+                            ->label('Tersedia untuk ujian')
+                            ->boolean(),
+                        TextEntry::make('payload')
+                            ->label('Soal')
+                            ->html()
+                            // use Filament\Support\Enums\TextSize;
+                            ->size(TextSize::Medium)
+                            ->columnSpanFull(),
+                        TextEntry::make('answers.option')
+                            ->hiddenLabel()
+                            ->listWithLineBreaks()
+                            ->bulleted()
+                            ->columnSpanFull(),
+                        TextEntry::make('corrects')
+                            ->label('Jawaban benar')
+                            ->state(fn (Question $record) =>
+                                $record->answers()
+                                    ->where('is_correct', true)
+                                    ->pluck('option')
+                                    ->toArray()
+                            )
+                            ->badge()
+                            ->color('success'),
+                    ])
             ]);
     }
 }
--- app/Filament/Resources/Questions/Tables/QuestionsTable.php
+++ app/Filament/Resources/Questions/Tables/QuestionsTable.php
@@ -2,12 +2,15 @@
 
 namespace App\Filament\Resources\Questions\Tables;
 
+use App\Models\Question;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Filters\TernaryFilter;
 use Filament\Tables\Table;
 
 class QuestionsTable
@@ -15,13 +18,29 @@ class QuestionsTable
     public static function configure(Table $table): Table
     {
         return $table
+            ->defaultSort('created_at', 'desc')
             ->columns([
+                TextColumn::make('No')
+                    ->rowIndex()
+                    ->width(40),
                 TextColumn::make('subject.name')
+                    ->label('Pelajaran')
                     ->searchable(),
                 TextColumn::make('score')
+                    ->label('Bobot')
+                    ->suffix(' poin')
                     ->numeric()
                     ->sortable(),
+                TextColumn::make('jawaban')
+                    ->state(function (Question $record) {
+                        $benar = $record->answers()
+                            ->where('is_correct', true)
+                            ->count();
+                        $opsi = $record->answers()->count();
+                        return $benar . '/' . $opsi;
+                    }),
                 IconColumn::make('is_active')
+                    ->label('Tersedia')
                     ->boolean(),
                 TextColumn::make('created_at')
                     ->dateTime()
@@ -33,7 +52,19 @@ class QuestionsTable
                     ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
-                //
+                // use Filament\Tables\Filters\SelectFilter;
+                SelectFilter::make('subject')
+                    ->relationship('subject', 'name')
+                    ->label('Mata pelajaran')
+                    ->multiple()
+                    ->native(false),
+                // use Filament\Tables\Filters\TernaryFilter;
+                TernaryFilter::make('is_active')
+                    ->label('Status soal')
+                    ->trueLabel('Tersedia untuk diujiankan')
+                    ->falseLabel('Tidak digunakan untuk ujian')
+                    ->placeholder('Pilih salah satu')
+                    ->native(false),
             ])
             ->recordActions([
                 ViewAction::make(),
